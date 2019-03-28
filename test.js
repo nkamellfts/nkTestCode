fe2917a5858a0f2249993133be978d33eec0b97d
nkamellfts <49045963+nkamellfts@users.noreply.github.com>
2019-03-28 16:13:49 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+const log = require('node-pretty-log'); // this line tells your program to include the node-pretty-log library we downloaded in step 4.
+  
+log('error', 'Uh Oh...'); // makes text more pretty
+log('warn', 'Warning...'); // makes text more pretty
+log('info', 'Info...'); // makes text more pretty
+log('success', 'Success...'); // makes text more pretty
